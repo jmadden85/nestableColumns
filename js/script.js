e78b51560d91b43f2753f66e76af9030185ee545
Joe Madden <joe@beardbro.com>
2014-01-27 14:26:41 -0500

    set up initial column structure and click events

--- js/script.js
+++ js/script.js
@@ -1,13 +1,20 @@
 (function () {
     var nestedColumns = {
         init : function () {
+            //store this as that
             var that = this;
+            //Set click event on all items
             $('.item').click(function () {
                 that.select(this);
             });
+            $('.addItem').click(function () {
+                that.addItem($(this).parents('ul'));
+            });
         },
+        //method for selecting an element
         select : function (item) {
-            var curr = this.curriculum;
+            var that = this;
+            var curr = that.curriculum;
             var thisId = $(item).attr('data-id');
             var itemColumn = $(item).parent().attr('data-colNum');
             var itemAncestry = $(item).attr('data-ancestry') || null;
@@ -15,19 +22,23 @@
             var thisItemTitle = $(item).children('h3').html();
             var itemParent;
 
+            //check if this item is active
+            //if it is do nothing
             if ($(item).hasClass('active')) {
                 return false;
+            //if it isn't active remove other columns after this one
             } else if ($(item).siblings().hasClass('active')) {
-                //go through other columns and remove them
+                $('[data-colNum="' + itemColumn + '"] ~ ul').remove();
             }
-
+            //remove active class from all siblings
             $(item).siblings().removeClass('active');
+            //add active class to this item
             $(item).addClass('active');
 
             //Check if the selected item has a ancestors
             if (itemAncestry) {
                 //Set item parent to the correct object based on ancestry
-                itemParent = this.getAncestry(itemAncestry);
+                itemParent = that.getAncestry(itemAncestry);
                 thisItem = itemParent[thisId];
             }
             //check if object has been defined or not yet
@@ -39,96 +50,139 @@
                 itemParent[thisId] = {};
                 thisItem = itemParent[thisId];
             }
-            console.log(curr);
+            //display next container
             this.showChildContainer(thisItem, itemColumn, thisItemTitle);
         },
-        showChildContainer : function (children, column, title) {
+        //function to show new container
+        showChildContainer : function (item, column, title) {
+            //set new column number to current column + 1
             var newCol = +column + 1;
-            var select = this.select;
-            $("<ul/>", {
-                    "class": "column",
-                    "data-colNum": newCol
+            var that = this;
+            //create column add attributes
+            $('<ul/>', {
+                    'class': 'column',
+                    'data-colNum': newCol
             }).appendTo('.nestableWrapper');
-            for (var i in children) {
-                var title = children[i].title;
-                var id = children[i].id;
-                var ancestry = children[i].ancestry;
+            var thisColumn = $('[data-colNum="' + newCol + '"]');
+            //add header to this column
+            $('<li class="head">' +
+                '<span id="sectionTitle">Modules</span>' +
+                '<div class="addItem">+</div>' +
+                '</li>').appendTo(thisColumn);
+            //get clicked item children and build list items from them
+            for (var i in item.children) {
+                var title = item.children[i].title;
+                var id = item.children[i].id;
+                var ancestry = item.children[i].ancestry;
                 $('<li class="item" data-ancestry="' + ancestry + '" data-id="' + id + '">' +
                     '<h3>' + title + '</h3>' +
                     '</li>').appendTo('[data-colNum="' + newCol + '"]');
             }
-            $('[data-colNum="' + newCol + '"] li').click(function () {
-                select(this)
+            //add select method to all new items
+            //add add item method to new column
+            var thisColumnItems = thisColumn.find('.item');
+            thisColumnItems.click(function () {
+                that.select(this);
+            });
+            thisColumn.find('.addItem').click(function () {
+                that.addItem($(this).parents('ul'));
             });
         },
+        //method to add new item
+        addItem : function (el) {
+            console.log(el.attr('data-colnum'));
+        },
+        //get ancestors of clicked item based on this ancestry
         getAncestry : function (ancestry) {
-            var curr = this.curriculum;
+            var that = this;
+            var curr = that.curriculum;
             var ancestorArray = ancestry.split('.');
             var thisItem = curr;
+            //loop through ancestry array
             for (var i = 0, l = ancestorArray.length; i < l; i++) {
-                thisItem = thisItem[i];
+                thisItem = thisItem[ancestorArray[i]];
             }
             return thisItem;
         },
         curriculum : {
             1 : {
-                22 : {
-                    title : 'testing',
-                    id : 22,
-                    ancestry : '1'
-                },
-                40 : {
-                    title : 'abcdefg',
-                    id : 40,
-                    ancestry : '1'
+                children : {
+                    22 : {
+                        title : 'testing',
+                        id : 22,
+                        ancestry : '1',
+                        children : {}
+                    },
+                    40 : {
+                        title : 'abcdefg',
+                        id : 40,
+                        ancestry : '1',
+                        children : {}
+                    }
                 }
             },
             2 : {
-                22 : {
-                    title : 'testing',
-                    id : 22,
-                    ancestry : '1'
-                },
-                40 : {
-                    title : 'abcdefg',
-                    id : 40,
-                    ancestry : '2'
+                children : {
+                    22 : {
+                        title : 'arggg',
+                        id : 22,
+                        ancestry : '2',
+                        children : {}
+                    },
+                    40 : {
+                        title : 'garggg',
+                        id : 40,
+                        ancestry : '2',
+                        children : {}
+                    }
                 }
             },
             3 : {
-                22 : {
-                    title : 'testing',
-                    id : 22,
-                    ancestry : '1'
-                },
-                40 : {
-                    title : 'abcdefg',
-                    id : 40,
-                    ancestry : '3'
+                children : {
+                    22 : {
+                        title : 'adsfaewf',
+                        id : 22,
+                        ancestry : '3',
+                        children : {}
+                    },
+                    40 : {
+                        title : '233233',
+                        id : 40,
+                        ancestry : '3',
+                        children : {}
+                    }
                 }
             },
             4 : {
-                22 : {
-                    title : 'testing',
-                    id : 22,
-                    ancestry : '1'
-                },
-                40 : {
-                    title : 'abcdefg',
-                    id : 40,
-                    ancestry : '4'
+                children : {
+                    22 : {
+                        title : '222',
+                        id : 22,
+                        ancestry : '4',
+                        children : {}
+                    },
+                    40 : {
+                        title : 'ddd',
+                        id : 40,
+                        ancestry : '4',
+                        children : {}
+                    }
                 }
             },
             5 : {
-                22 : {
-                    title : 'testing',
-                    id : 22,
-                    ancestry : '1'
-                },
-                40 : {
-                    title : 'abcdefg',
-                    id : 40,
-                    ancestry : '5'
+                children : {
+                    22 : {
+                        title : 'abc',
+                        id : 22,
+                        ancestry : '5',
+                        children : {}
+                    },
+                    40 : {
+                        title : '453',
+                        id : 40,
+                        ancestry : '5',
+                        children : {}
+                    }
                 }
             }
         }
